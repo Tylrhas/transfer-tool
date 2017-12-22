0efe683804068a1b2f8643d35453cd95adcd82e5
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2017-12-22 10:48:54 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require('express')
+var fs = require("fs")
+var path = require("path")
+var zipFolder = require('zip-folder');
+var urls = require(path.join(__dirname, 'assets.json'))
+var app = express()
+request = require('request')
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/', function (req, res) {
+    dirName = createDir();
+    for (i = 0; i < Object.keys(urls.assets).length; i++) {
+        //console.log(urls.assets[i]['secure_url']);
+        downloadImages(urls.assets[i]['secure_url'],dirName);
+    }
+
+    // zipFolder('/path/to/the/folder', '/path/to/archive.zip', function(err) {
+    //     if(err) {
+    //         console.log('oh no!', err);
+    //     } else {
+    //         console.log('EXCELLENT');
+    //     }
+    // });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Reporting Server is running on port', app.get('port'))
+});
+
+function downloadImages(url, dirName) {
+
+    var download = function (uri, filename, callback) {
+        request.head(uri, function (err, res, body) {
+            console.log('content-type:', res.headers['content-type']);
+            console.log('content-length:', res.headers['content-length']);
+
+            request(uri).pipe(fs.createWriteStream('assets/'+dirName+"/"+filename)).on('close', callback);
+        });
+    };
+    download(url, getFileName(url), function () {
+        console.log('done');
+    });
+
+}
+
+function getFileName(url) {
+    urlSplit = url.split("/");
+    index = urlSplit.length - 1;
+    return urlSplit[index];
+}
+
+function createDir(){
+    var date = new Date();
+    var datetime = date.getFullYear()+"-"+ date.getDay()+"-"+date.getMonth()+"-"+date.getTime();
+    var dir = 'asset_download-'+datetime;
+    if (!fs.existsSync('assets/'+dir)){
+        fs.mkdirSync('assets/'+dir);
+    }
+    return dir;
+}
