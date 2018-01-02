e975ddded23373f3b498c3a8d3436e2c34ff982b
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-01-02 13:15:29 -0800

    init commit

--- server.js
+++ server.js
@@ -5,45 +5,52 @@ var zipFolder = require('zip-folder');
 var bodyParser = require('body-parser');
 var app = express()
 request = require('request')
+var URL = 'localhost:5000'
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.set('port', (process.env.PORT || 5000))
+//set the contents of the public folder to be accessable to the public
+app.use(express.static(__dirname + '/public'))
 
 app.post('/downloadassets', function (req, res) {
-    res.send(req.body);
     urls = req.body
     dirName = createDir();
     for (i = 0; i < urls.length; i++) {
         //console.log(urls.assets[i]['secure_url']);
-        downloadImages(urls[i],dirName);
+        downloadImages(urls[i],dirName, i);
     }
-
-    // zipFolder('/path/to/the/folder', '/path/to/archive.zip', function(err) {
-    //     if(err) {
-    //         console.log('oh no!', err);
-    //     } else {
-    //         console.log('EXCELLENT');
-    //     }
-    // });
+    //listen for event
+    zipLocation = URL+'/assets/'+dirName+'.zip';
+        res.send(zipLocation);
 });
 
 app.listen(app.get('port'), function () {
     console.log('Reporting Server is running on port', app.get('port'))
 });
 
-function downloadImages(url, dirName) {
+function downloadImages(url, dirName, i) {
 
     var download = function (uri, filename, callback) {
         request.head(uri, function (err, res, body) {
             console.log('content-type:', res.headers['content-type']);
             console.log('content-length:', res.headers['content-length']);
 
-            request(uri).pipe(fs.createWriteStream('assets/'+dirName+"/"+filename)).on('close', callback);
+            request(uri).pipe(fs.createWriteStream('public/assets/'+dirName+"/"+filename)).on('close', callback);
         });
     };
     download(url, getFileName(url), function () {
         console.log('done');
+        if (i === urls.length -1){
+            console.log('all done');
+            zipFolder('./public.assets/'+dirName, './public/assets/'+dirName+'.zip', function(err) {
+                if(err) {
+                    console.log('oh no!', err);
+                } else {
+                    console.log('EXCELLENT');
+                }
+            });
+        }
     });
 
 }
@@ -58,8 +65,8 @@ function createDir(){
     var date = new Date();
     var datetime = date.getFullYear()+"-"+ date.getDay()+"-"+date.getMonth()+"-"+date.getTime();
     var dir = 'asset_download-'+datetime;
-    if (!fs.existsSync('assets/'+dir)){
-        fs.mkdirSync('assets/'+dir);
+    if (!fs.existsSync('public/assets/'+dir)){
+        fs.mkdirSync('public/assets/'+dir);
     }
     return dir;
 }
