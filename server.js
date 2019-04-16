1485e32a816c931dfda1a28670dc301832887e89
Tyler Hasenoehrl <tylerhasenoehrl@01809-MBP15.local>
2019-04-16 08:58:32 -0700

    remove old code

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 var express = require('express')
 var fs = require("fs")
-var path = require("path")
-var zipFolder = require('zip-folder')
+const Path = require('path')
+var zipFolder = require('zip-a-folder')
 var bodyParser = require('body-parser')
-var schedule = require('node-schedule')
 var request = require('request')
 var app = express()
-var rimraf = require('rimraf')
-var EventEmitter = require('events')
-
-class MyEmitter extends EventEmitter { }
-const myEmitter = new MyEmitter();
 var URL = process.env.appURL
 //URL = 'test.com'
 
@@ -22,113 +16,60 @@ appSetUp()
 //set the contents of the public folder to be accessable to the public
 app.use(express.static(__dirname + '/public'))
 
-app.post('/downloadassets', function (req, res) {
-    urls = req.body;
-    alreadyDownloaded = [];
-    dirName = createDir();
-
-    for (i = 0; i < urls.length; i++) {
-        console.log('i is '+ i);
-        downloadImages(urls[i], dirName, urls.length, alreadyDownloaded);
-    }
-    //listen for event
-    myEmitter.once('zipComplete', () => {
-        zipLocation = URL + '/assets/' + dirName + '.zip';
-        res.json({
-            'url': zipLocation,
-            'status': 'sucess'
-        });
-    })
-});
+app.post('/downloadassets', async function (req, res) {
+  let urls = req.body
+  let dirName = createDir()
+  await downloadImages(urls, dirName)
+  await zipFolder.zip(`public/assets/${dirName}`, `public/assets/${dirName}.zip`)
+  let zipLocation = `${URL}/assets/${dirName}.zip`
+  res.json({
+    'url': zipLocation,
+    'status': 'sucess'
+  })
+})
 
 app.listen(app.get('port'), function () {
-    console.log('Reporting Server is running on port', app.get('port'))
-});
+  console.log('Reporting Server is running on port', app.get('port'))
+})
 
 function appSetUp() {
-    //create the public folder and the assets folder 
-    if (!fs.existsSync('public')) {
-        fs.mkdirSync('public');
-    }
+  //create the public folder and the assets folder 
+  if (!fs.existsSync('public')) {
+    fs.mkdirSync('public');
+  }
 
-    if (!fs.existsSync('public/assets')) {
-        fs.mkdirSync('public/assets');
-    }
+  if (!fs.existsSync('public/assets')) {
+    fs.mkdirSync('public/assets');
+  }
 }
-
-function downloadImages(url, dirName, numberOfFiles, alreadyDownloaded) {
-    var download = function (uri, filename, callback) {
-        request.head(uri, function (err, res, body) {
-            request(uri).pipe(fs.createWriteStream('./public/assets/' + dirName + "/" + filename)).on('close', callback);
-        });
-    };
-    download(url, getFileName(url, dirName, alreadyDownloaded), function () {
-        dir = './public/assets/' + dirName;
-        fs.readdir(dir, (err, files) => {
-            if (numberOfFiles === files.length) {
-                console.log('all done');
-                zipFolder('./public/assets/' + dirName, './public/assets/' + dirName + '.zip', function (err) {
-                    if (err) {
-                        console.log('oh no!', err);
-                    } else {
-                        console.log('complete')
-                        myEmitter.emit('zipComplete');
-                    }
-                });
-            }
-        })
-    });
+function downloadImages(urls, dirName) {
+  let images = []
+  for (i = 0; i < urls.length; i++) {
+    images.push(downloadImage(urls[i], dirName))
+  }
+  return Promise.all(images)
+}
+function downloadImage(url, dirName) {
+  let filename = getFileName(url)
+  const path = Path.resolve(__dirname, 'public', 'assets', `${dirName}`, `${filename}`)
+  return request(url).pipe(fs.createWriteStream(path))
 
 }
 
-function getFileName(url, dirName, alreadyDownloaded) {
-    //split the url to just get the file name
-    urlSplit = url.split("/");
-    index = urlSplit.length - 1;
-    //strip off the id that cloudinary adds
-    splitFileName = urlSplit[index].split('.');
-    newfile = splitFileName[0].substring(0, splitFileName[0].length - 7);
-    //check if the filename is in use
-    console.log(newfile + '.' + splitFileName[1]);
-    console.log(fileExists(newfile + '.' + splitFileName[1], alreadyDownloaded));
-    if (fileExists(newfile + '.' + splitFileName[1], alreadyDownloaded)) {
-        e = 2;
-        while (fileExists(newfile + '-' + e + '.' + splitFileName[1], alreadyDownloaded)) {
-            console.log('increasing i')
-            e++
-        }
-        //add the file extension back in
-        renamedFile = newfile + '-' + e + '.' + splitFileName[1];
-        alreadyDownloaded.push(renamedFile);
-        return renamedFile;
-    }
-    else {
-        //add the file extension back in
-        fileWithFormat = newfile + '.' + splitFileName[1];
-        //add file to download
-        alreadyDownloaded.push(fileWithFormat);
-        return fileWithFormat;
-    }
-
+function getFileName(url) {
+  //split the url to just get the file name
+  let urlSplit = url.split("/")
+  let index = urlSplit.length - 1
+  let newfileName = urlSplit[index]
+  return newfileName
 }
 
 function createDir() {
-    var date = new Date();
-    var datetime = date.getFullYear() + "-" + date.getDay() + "-" + date.getMonth() + "-" + date.getTime();
-    var dir = 'asset_download-' + datetime;
-    if (!fs.existsSync('./public/assets/' + dir)) {
-        fs.mkdirSync('./public/assets/' + dir);
-    }
-    return dir;
+  var date = new Date();
+  var datetime = `${date.getFullYear()}-${date.getDay()}-${date.getMonth()}-${date.getTime()}`
+  var dir = `asset_download-${datetime}`
+  if (!fs.existsSync('./public/assets/' + dir)) {
+    fs.mkdirSync('./public/assets/' + dir);
+  }
+  return dir;
 }
-
-function fileExists(fileName, alreadyDownloaded) {
-    return alreadyDownloaded.includes(fileName);
-}
-
-schedule.scheduleJob('0 0 * * *', function(){
-    console.log('removing the dir');
-    lrimraf('./public/assets/', function () {
-        appSetUp()
-     });
-  });
